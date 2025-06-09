1d9b257d4aab568510fe7d231fbaba4ff493e38a
Jonathan Pearson <103967053+Galondor@users.noreply.github.com>
2025-06-09 17:32:36 -0400

    Carpet + Plank Comparison & Clarification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import loading from "./assets/loader.svg";
 import { Analytics } from "@vercel/analytics/react"
 import "./App.css";
 import "./assets/Biggby.jpg";
+import "./assets/carpet.png";
+import "./assets/plank.png";
 
 function App() {
   const [showResults, setShowResults] = useState(false);
@@ -63,7 +65,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="App">
       <nav>
         <div className='row'></div>
       </nav>
@@ -72,7 +74,8 @@ function App() {
           <h1 className='title'>YESUP Flooring Quiz</h1>
           <p className='intro_para'>
             The free quiz that will help you find your perfect floor. <br />{" "}
-            Take the quiz below to begin your journey!
+            Take the quiz below to begin your journey! <br />{" "}
+            Scroll down to get informed about carpet and waterproof plank flooring.
           </p>
         </div>
       </header>
@@ -93,18 +96,18 @@ function App() {
               {selectedQuizType === "Both" ? (
                 <>
                   <h2 className='results_score'>Thank you for taking our quiz. <br></br> Here are your scores:</h2>
-                  <h2 className='results_score'>LVP Score: <b>{scoreLVP}</b></h2>
-                  <h2 className='results_score'>CPT Score: <b>{scoreCPT}</b></h2>
+                  <h2 className='results_score'>Waterproof Plank Flooring Score: <b>{scoreLVP}</b></h2>
+                  <h2 className='results_score'>Carpet Score: <b>{scoreCPT}</b></h2>
                 </>
               ) : selectedQuizType === "LVP" ? (
                 <>
                 <h2 className='results_score'>Thank you for taking our quiz. <br></br> Here is your score:</h2>
-                <h2 className='results_score'>LVP Score: <b>{scoreLVP}</b></h2>
+                <h2 className='results_score'>Waterproof Plank Flooring Score: <b>{scoreLVP}</b></h2>
                 </>
               ) : (
                 <>
                 <h2 className='results_score'>Thank you for taking our quiz. <br></br> Here is your score:</h2>
-                <h2 className='results_score'>CPT Score: <b>{scoreCPT}</b></h2>
+                <h2 className='results_score'>Carpet Score: <b>{scoreCPT}</b></h2>
                 </>
               )}
               <div className='results_desc'>
@@ -157,6 +160,62 @@ function App() {
               </ul>
             </>
           )}
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <h1 className="procon_title">What are the differences between carpet and waterproof plank flooring?</h1>
+          <div className="procon_container">
+            <div className="procon">
+              <h2>Carpet</h2>
+              <p className="procon_text">Carpet is a soft flooring option that provides warmth and comfort. However, carpet can be more difficult to clean and maintain than other flooring options.</p>
+              <div className="procon_list">
+                <div className="procon_pros">
+                  <h2>Pros</h2>
+                  <ul className="procon_list_items">
+                    <li>Soft and comfortable.</li>
+                    <li>Wide variety of styles and textures.</li>
+                    <li>Warmer underfoot.</li>
+                    <li>Quiet.</li>
+                  </ul>
+                </div>
+                <div className="procon_cons">
+                  <h2>Cons</h2>
+                  <ul className="procon_list_items">
+                    <li>Can be difficult to clean and maintain.</li>
+                    <li>Not waterproof, can stain easily.</li>
+                    <li>May require professional cleaning.</li>
+                    <li>Typically doesn't last as long.</li>
+                  </ul>
+                </div>
+              </div>
+              <img className="procon_img" src={require("./assets/carpet.png")} alt="Carpet"/>
+            </div>
+            <div className="procon">
+              <h2>Waterproof Plank Flooring</h2>
+              <p className="procon_text">Waterproof plank flooring also known as Luxury Vinyl Plank (LVP) is a durable and waterproof option that mimics the look of hardwood. It is easy to clean and maintain, making it a practical choice for high-traffic areas and homes with pets.</p>
+              <div className="procon_list">
+                <div className="procon_pros">
+                  <h2>Pros</h2>
+                  <ul className="procon_list_items">
+                    <li>Waterproof and durable.</li>
+                    <li>Easy to clean and maintain.</li>
+                    <li>Available in a variety of styles and colors.</li>
+                  </ul>
+                </div>
+                <div className="procon_cons">
+                  <h2>Cons</h2>
+                  <ul className="procon_list_items">
+                    <li>More expensive than other flooring options.</li>
+                    <li>Colder underfoot.</li>
+                    <li>Not much sound insulation.</li>
+                    <li>Not as comfortable as carpet.</li>
+                  </ul>
+                </div>
+              </div>
+              <img className="procon_img" src={require("./assets/plank.png")} alt="Waterproof Plank Flooring"/>
+            </div>
+          </div>
         </div>
       </div>
       <Analytics />
